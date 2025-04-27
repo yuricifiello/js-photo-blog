933f9e3bc679404db0e0d14fa696e5bc0a262262
yuricifiello <yuricifiello@gmail.com>
2025-04-27 19:16:57 +0200

    add script js

---
+++ script.js
@@ -0,0 +1,16 @@
+axios
+  .get("https://lanciweb.github.io/demo/api/pictures/")
+  .then((response) => {
+    const data = response.data;
+
+    const card = document.querySelector(".card");
+    const immagine = card.querySelector(".immagine");
+    const descrizione = card.querySelector(".descrizione");
+
+    immagine.src = data[0].url;
+    immagine.alt = data[0].title;
+    descrizione.textContent = data[0].title;
+  })
+  .catch((error) => {
+    console.error("Errore nel caricamento dei dati:", error);
+  });
