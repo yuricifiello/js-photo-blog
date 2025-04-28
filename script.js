7077587e573801f18ed7802f7fb09a0be2b8260c
yuricifiello <yuricifiello@gmail.com>
2025-04-28 15:34:52 +0200

    add image with js

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
 axios
   .get("https://lanciweb.github.io/demo/api/pictures/")
   .then((response) => {
-    const data = response.data;
+    const immagini = response.data;
+    const container = document.querySelector(".row");
 
-    const card = document.querySelector(".card");
-    const immagine = card.querySelector(".image");
-    const descrizione = card.querySelector(".description");
+    immagini.forEach((immagine) => {
+      const col = document.createElement("div");
+      col.classList.add("col");
 
-    immagine.src = data[0].url;
-    immagine.alt = data[0].title;
-    descrizione.textContent = data[0].title;
+      const card = document.createElement("div");
+      card.classList.add("card");
+
+      // Creo la puntina
+      const puntina = document.createElement("img");
+      puntina.src = "../img/pin.svg";
+      puntina.alt = "puntina";
+      puntina.classList.add("puntina");
+
+      // Creo l'immagine principale
+      const imgElement = document.createElement("img");
+      imgElement.src = immagine.url;
+      imgElement.alt = immagine.title;
+      imgElement.classList.add("foto");
+
+      // Creo la descrizione
+      const descrizione = document.createElement("p");
+      descrizione.textContent = immagine.title;
+      descrizione.classList.add("descrizione");
+
+      // Inserisco tutto nella card
+      card.appendChild(puntina);
+      card.appendChild(imgElement);
+      card.appendChild(descrizione);
+
+      // Inserisco la card nella colonna
+      col.appendChild(card);
+
+      // Inserisco la colonna nella riga
+      container.appendChild(col);
+    });
   })
   .catch((error) => {
-    console.error("Errore nel caricamento dei dati:", error);
+    console.error("Errore nel caricamento delle immagini:", error);
   });
